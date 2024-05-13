0ffdf8437ec1a5e9e258c18d817cee95eff66f1a
Hans Schneebauer <hans.schneebauer@arm.com>
2024-05-13 21:56:38 +0000

    Add bash script shebang; Quote paths; Order arguments;

--- docker_base/armgcc_install.sh
+++ docker_base/armgcc_install.sh
@@ -1,3 +1,5 @@
-wget https://artifacts.tools.arm.com/arm-none-eabi-gcc/12.3.1/arm-gnu-toolchain-x86_64-arm-none-eabi.tar.bz2 -nv
-tar -xf arm-gnu-toolchain-x86_64-arm-none-eabi.tar.bz2 -C /home/arm_mlops_docker
-chown -R arm_mlops_docker /home/arm_mlops_docker/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/
+#!/bin/bash
+
+wget -nv 'https://artifacts.tools.arm.com/arm-none-eabi-gcc/12.3.1/arm-gnu-toolchain-x86_64-arm-none-eabi.tar.bz2'
+tar -xf 'arm-gnu-toolchain-x86_64-arm-none-eabi.tar.bz2' -C '/home/arm_mlops_docker'
+chown -R arm_mlops_docker:arm_mlops_docker '/home/arm_mlops_docker/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/'
--- docker_base/armllvm_install.sh
+++ docker_base/armllvm_install.sh
@@ -1,5 +1,7 @@
-wget https://github.com/ARM-software/LLVM-embedded-toolchain-for-Arm/releases/download/release-16.0.0/LLVMEmbeddedToolchainForArm-16.0.0-Linux-x86_64.tar.gz -nv
-tar -xf LLVMEmbeddedToolchainForArm-16.0.0-Linux-x86_64.tar.gz -C /home/arm_mlops_docker
-apt install -y libtinfo5
+#!/bin/bash
+
+wget -nv 'https://github.com/ARM-software/LLVM-embedded-toolchain-for-Arm/releases/download/release-16.0.0/LLVMEmbeddedToolchainForArm-16.0.0-Linux-x86_64.tar.gz'
+tar -xf 'LLVMEmbeddedToolchainForArm-16.0.0-Linux-x86_64.tar.gz' -C '/home/arm_mlops_docker'
+apt-get install -y libtinfo5
 #echo 'export "/home/arm_mlops_docker/LLVMEmbeddedToolchainForArm-16.0.0-Linux-x86_64/bin:$PATH"' >> /home/arm_mlops_docker/.bashrc
-chown -R arm_mlops_docker /home/arm_mlops_docker/LLVMEmbeddedToolchainForArm-16.0.0-Linux-x86_64
+chown -R arm_mlops_docker:arm_mlops_docker '/home/arm_mlops_docker/LLVMEmbeddedToolchainForArm-16.0.0-Linux-x86_64'
